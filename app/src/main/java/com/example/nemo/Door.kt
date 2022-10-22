bf7fecea7de578b19caccf325ec29eb4488b13c2
Deya Hazra <97392288+deyahazra@users.noreply.github.com>
2022-10-22 11:49:53 +0530

    ok

--- app/src/main/java/com/example/nemo/Door.kt
+++ app/src/main/java/com/example/nemo/Door.kt
@@ -1,16 +1,44 @@
 package com.example.nemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.Window
+import android.widget.Button
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 
 class Door : AppCompatActivity() {
+    private var vView: VideoView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         actionBarHide()
         statusBarColour()
         setContentView(R.layout.activity_door)
+        val vView = findViewById<VideoView>(R.id.videoView)
+        vView.setVideoPath("android.resource://" + packageName + "/" + R.raw.clip)
+        val mediaController = MediaController(this)
+        vView.setMediaController(mediaController)
+        mediaController.setAnchorView(vView)
+        val dButton : Button = findViewById<Button>(R.id.WaterButton)
+
+        dButton.setOnTouchListener { v, event ->
+            val action = event.action
+            when(action){
+
+                MotionEvent.ACTION_DOWN -> {
+                    vView?.start()
+                }
+                MotionEvent.ACTION_UP -> {
+                    vView.stopPlayback()
+                }
+            }
+            true
+        }
+
     }
+
 
 
 
