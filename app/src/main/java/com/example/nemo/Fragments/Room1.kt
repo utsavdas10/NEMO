2be58743f79e7833c5bf0930192f8e4ca3366f14
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2022-10-25 16:37:15 +0530

    graph

--- app/src/main/java/com/example/nemo/Fragments/Room1.kt
+++ app/src/main/java/com/example/nemo/Fragments/Room1.kt
@@ -1,12 +1,20 @@
 package com.example.nemo.Fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.nemo.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class Room1 : Fragment() {
@@ -67,5 +75,62 @@ class Room1 : Fragment() {
         room1gauge2text2.animate().alpha(1f).duration = 2000;
         room1gauge2text2.translationY = 120F
         room1gauge2text2.animate().alpha(1f).translationYBy(-120F).duration = 1800
+
+
+        val lineChart : LineChart = view.findViewById(R.id.lineChart)
+        val lineDataSet : LineDataSet = LineDataSet(lineChartDataSet(), "Temperature")
+        val iLineDataSet : ArrayList<ILineDataSet> = ArrayList<ILineDataSet>()
+        iLineDataSet.add(lineDataSet)
+
+        val lineData : LineData = LineData(iLineDataSet)
+        lineChart.data = lineData
+        lineChart.invalidate()
+        lineChart.legend.isEnabled = false
+
+        lineChart.xAxis.apply {
+            setDrawGridLines(false)
+            axisMinimum = -0.5f
+            axisMaximum = 7.5f
+            position = XAxis.XAxisPosition.BOTTOM
+            textSize = 10f
+            textColor = context?.let { ContextCompat.getColor(it, R.color.teal_200) }!!
+            axisLineColor = context?.let { ContextCompat.getColor(it, R.color.teal_200) }!!
+        }
+        lineChart.axisLeft.apply {
+            isEnabled = true
+            setDrawGridLines(false)
+            axisMinimum = 15f
+            axisMaximum = 40f
+            textSize = 10f
+            textColor = context?.let { ContextCompat.getColor(it, R.color.teal_200) }!!
+            axisLineColor = context?.let { ContextCompat.getColor(it, R.color.teal_200) }!!
+        }
+        lineChart.axisRight.apply {
+            isEnabled = false
+        }
+        lineDataSet.apply {
+            color = context?.let { ContextCompat.getColor(it, R.color.teal_200) }!!
+            valueTextColor = context?.let { ContextCompat.getColor(it, R.color.teal_200) }!!
+            lineWidth = 4f
+            setDrawCircles(false)
+            setDrawHighlightIndicators(false)
+            mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+            setDrawFilled(true)
+            fillDrawable = context?.let { ContextCompat.getDrawable(it, R.drawable.bg_line) }!!
+        }
+    }
+    private fun lineChartDataSet() : ArrayList<Entry>
+    {
+        val dataSet = ArrayList<Entry>()
+        dataSet.add(Entry(0f,20f))
+        dataSet.add(Entry(1f,22f))
+        dataSet.add(Entry(2f,30f))
+        dataSet.add(Entry(3f,28f))
+        dataSet.add(Entry(4f,25f))
+        dataSet.add(Entry(5f,32f))
+        dataSet.add(Entry(6f,35f))
+        dataSet.add(Entry(7f,27f))
+
+        return dataSet
     }
 }
